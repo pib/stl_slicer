6f1b16ec031b3d4e13eed2f3ac6c4cb7b9e7bc3a
Paul Bonser <pib@paulbonser.com>
2011-10-30 14:36:45 -0500

    Combine lines into paths, ignore lines that start and end at the same spot

--- slicer.py
+++ slicer.py
@@ -9,8 +9,11 @@ X = 0
 Y = 1
 Z = 2
 
+
 def find_line_plane_intersection(a, b, z):
-    """ Find where a line between two points intersects a given axis-aligned plane """
+    """ Find where a line between two points intersects a given
+    axis-aligned plane
+    """
     dx = a[X] - b[X]
     dy = a[Y] - b[Y]
     dz = a[Z] - b[Z]
@@ -18,8 +21,8 @@ def find_line_plane_intersection(a, b, z):
 
     xi = z_z0 * (dx / dz) + b[X]
     yi = z_z0 * (dy / dz) + b[Y]
-    #print '% 6.2f, % 6.2f, % 6.2f to % 6.2f, % 6.2f, % 6.2f -> % 6.2f, % 6.2f' % (a[0], a[1], a[2], b[0], b[1], b[2], xi, yi)
     return (xi, yi)
+
 
 def tri_above_below(tri, z):
     # FIXME: how to handle when all Zs are equal to z
@@ -32,6 +35,7 @@ def tri_above_below(tri, z):
             above.append(point)
     return above, below
 
+
 def slice_shape_at(facets, z):
     lines = []
     for tri in facets:
@@ -43,8 +47,92 @@ def slice_shape_at(facets, z):
         for a in above:
             for b in below:
                 line.append(find_line_plane_intersection(a, b, z))
-        lines.append(line)
+        if line[0] != line[1]:
+            lines.append(line)
     return lines
+
+
+class LineMap(object):
+    def __init__(self, lines):
+        map = {}
+        for line in lines:
+            for point in line:
+                key = self._key(point)
+                line_set = map.setdefault(key, [])
+                line_set.append(line)
+        self._map = map
+
+    def _key(self, point):
+        return '{0:.8},{1:.8}'.format(*point)
+
+    def has_lines(self):
+        if self._map:
+            return True
+        else:
+            return False
+
+    def _pop_other_line(self, key, line):
+        for point in line:
+            if self._key(point) == key:
+                continue
+            other_point = point
+            break
+
+        key = self._key(other_point)
+        lines = self._map[key]
+        lines.remove(line)
+        if len(lines) == 0:
+            del self._map[key]
+
+        return other_point
+
+    def take_line(self):
+        """ Take an arbitrary line out of the internal map
+        """
+        for key, lines in self._map.iteritems():
+            line = lines.pop()
+            if len(lines) == 0:
+                del self._map[key]
+
+            self._pop_other_line(key, line)
+            return line
+
+    def next_point(self, point):
+        """ Return the next endpoint of an unused line which starts at
+        the given point.
+
+        Also removes the used line from the internal map.
+        """
+        key = self._key(point)
+        lines = self._map.get(key, None)
+        if lines is None:
+            return None
+
+        line = lines.pop()
+        if len(lines) == 0:
+            del self._map[key]
+
+        other_point = self._pop_other_line(key, line)
+
+        return other_point
+
+
+def path_lines(lines):
+    """ Convert a list of lines into paths
+    """
+    unused = LineMap(lines)
+
+    paths = []
+    while unused.has_lines():
+        path = unused.take_line()
+
+        point = unused.next_point(path[-1])
+        while point:
+            path.append(point)
+            point = unused.next_point(point)
+        paths.append(path)
+    return paths
+
 
 def main(args):
     facets = parse(open(args.file, 'rb'))
@@ -62,11 +150,12 @@ def main(args):
     z = minz
     while z <= maxz:
         lines = slice_shape_at(facets, z)
-        svg.write_layer(f, lines)
+        paths = path_lines(lines)
+        svg.write_layer_paths(f, paths)
         z += args.thickness
     svg.end_file(f)
     f.close()
-    
+
 
 if __name__ == '__main__':
     import argparse
--- svg.py
+++ svg.py
@@ -4,7 +4,14 @@ def start_file(f):
 def end_file(f):
         f.write('</svg>\n')
     
-def write_layer(f, lines):
+def write_layer_lines(f, lines):
     for line in lines:
         f.write('<line x1="%f" y1="%f" x2="%f" y2="%f" stroke-width=".5" stroke="red" />\n' % (
                 line[0][0], line[0][1], line[1][0], line[1][1]))
+
+def write_layer_paths(f, paths):
+    for path in paths:
+        f.write('<path d="M %f %f' % path[0])
+        for point in path[1:]:
+            f.write(' L %f %f' % point)
+        f.write('" />\n')
