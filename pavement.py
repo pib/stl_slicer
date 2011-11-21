25cedb9f8d5825cf8a2e3e53b08785718a6f7f6e
Paul Bonser <pib@paulbonser.com>
2011-11-20 21:21:21 -0600

    started on paver setup

---
+++ pavement.py
@@ -0,0 +1,11 @@
+from paver.easy import *
+from paver.setuputils import setup
+
+setup(
+    name='slicer',
+    packages=['slicer'],
+    version='0.1',
+    url='http://probablyprogramming.com',
+    author='Paul Bonser',
+    author_email='misterpib@gmail.com'
+)
