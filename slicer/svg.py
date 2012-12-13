b0af6044a067502a142d70559a150d5bbc14a959
Paul Bonser <pib@paulbonser.com>
2012-12-13 13:53:35 -0600

    Create one number per path, and group numbers and paths together

--- slicer/svg.py
+++ slicer/svg.py
@@ -1,5 +1,8 @@
 def start_file(f, width, height):
-    f.write('<svg xmlns="http://www.w3.org/2000/svg">\n')
+    f.write("""<svg xmlns="http://www.w3.org/2000/svg"
+      xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+      xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape">\n""")
+    f.write('<sodipodi:namedview units="mm" />\n')
 
 
 def end_file(f):
@@ -13,13 +16,25 @@ def write_layer_lines(f, lines):
 
 
 def write_layer_paths(f, paths, number=None):
-    f.write('<g>\n')
-    if number is not None:
-        f.write('<text text-anchor="middle" font-size="2" fill="none" stroke-width="0.1" stroke="blue">%d</text>\n' % number)
+    f.write(
+        '<g inkscape:groupmode="layer" inkscape:label="Layer {0}" id="{0}">\n'.format(
+            number))
 
     for path in paths:
-        f.write('<path fill="none" stroke-width="0.1mm" stroke="red" d="M %fmm %fmm' % path[0])
+        if number is not None:
+            f.write('<g>\n')
+            x_avg = sum(d[0] for d in path) / len(path)
+            y_avg = sum(d[1] for d in path) / len(path)
+            f.write('<text text-anchor="middle" '
+                    'font-size="2" fill="none" stroke-width="0.1" stroke="blue" '
+                    'x="%s" y="%s"'
+                    '>%d</text>\n' % (x_avg, y_avg, number))
+
+        f.write('<path fill="none" stroke-width="0.1" stroke="red" d="M %f %f' % path[0])
         for point in path[1:]:
-            f.write(' L %fmm %fmm' % point)
+            f.write(' L %f %f' % point)
         f.write('" />\n')
+
+        if number is not None:
+            f.write('</g>\n')
     f.write('</g>\n')
